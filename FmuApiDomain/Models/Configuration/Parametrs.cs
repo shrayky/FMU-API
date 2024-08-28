32b208cdd0d8515f9741763940195860f3617e18
Арсений Жарков <shrayky@au124.ru>
2024-08-28 12:33:04 +0700

    версия сборки 6

--- FmuApiDomain/Models/Configuration/Parametrs.cs
+++ FmuApiDomain/Models/Configuration/Parametrs.cs
@@ -9,7 +9,7 @@ namespace FmuApiDomain.Models.Configuration
     {
         public string AppName { get; } = "FMU-API";
         public int AppVersion { get; set; } = 9;
-        public int Assembly { get; set; } = 5;
+        public int Assembly { get; set; } = 6;
         public string NodeName { get; set; } = string.Empty;
         public ServerConfig ServerConfig { get; set; } = new();
         public List<StringValue> HostsToPing { get; set; } = [];
