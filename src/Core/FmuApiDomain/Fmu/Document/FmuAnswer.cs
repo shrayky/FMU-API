f73ba2fccf7beef826ca4f9757df6cf4282f21b5
Арсений Жарков <shrayky@au124.ru>
2025-07-21 11:02:31 +0700

    - переместил информаицю о fmu-api ниже - добавил поле с версияей - новый метод FillFieldsFor6255 заполняет все поля включая инн и информаю о запросе в ЧЗ

--- src/Core/FmuApiDomain/Fmu/Document/FmuAnswer.cs
+++ src/Core/FmuApiDomain/Fmu/Document/FmuAnswer.cs
@@ -1,5 +1,6 @@
 ﻿// Ignore Spelling: Respinse Fmu
 
+using FmuApiDomain.Constants;
 using FmuApiDomain.Fmu.MarkCheckData;
 using FmuApiDomain.TrueApi.MarkData.Check;
 using System.Text.Json.Serialization;
@@ -13,13 +14,6 @@ namespace FmuApiDomain.Fmu.Document
         public string Error { get; set; } = string.Empty;
         public List<string> Stamps { get; set; } = [];
         public List<string> Marking_codes { get; set; } = [];
-        public List<Organization> Organizations { get; set; } = [];
-        [JsonPropertyName("fmu-api-offline")]
-        public bool Offline { get; set; } = false;
-        [JsonPropertyName("fmu-api-localModul")]
-        public bool OfflineRegime { get; set; } = false;
-        [JsonPropertyName("fmu-api-printgroup")]
-        public int PrintGroupCode { get; set; } = 0;
         [JsonPropertyName("truemark_response")]
         public CheckMarksDataTrueApi Truemark_response { get; set; } = new();
         [JsonPropertyName("truemark_responses")]
@@ -28,7 +22,14 @@ namespace FmuApiDomain.Fmu.Document
         public List<CheckMarksDataTrueApi> OffLineTrueMarkResponses { get; set; } = [];
         [JsonPropertyName("dmdk_responses")]
         public List<CheckMarksDataTrueApi> DmdkResponses { get; set; } = [];
-
+        [JsonPropertyName("fmu-api-offline")]
+        public bool Offline { get; set; } = false;
+        [JsonPropertyName("fmu-api-local-Module")]
+        public bool OfflineRegime { get; set; } = false;
+        [JsonPropertyName("fmu-api-print-group")]
+        public int PrintGroupCode { get; set; } = 0;
+        [JsonPropertyName("fmu-api-version")]
+        public string FmuApiVersion {  get; set; } = $"{ApplicationInformation.AppVersion}.{ApplicationInformation.Assembly}";
         [JsonIgnore]
         public bool IsEmpty => Stamps.Count == 0 && Marking_codes.Count == 0;
         public bool AllMarksIsSold() => Truemark_response.AllMarksIsSold();
@@ -51,6 +52,28 @@ namespace FmuApiDomain.Fmu.Document
                 Response = checkInformation
             };
             TrueMarkResponses.Add(checkResult);
+        }
+
+        public void FillFieldsFor6255(string Inn)
+        {
+            CheckInformation checkInformation = new();
+            checkInformation.Codes = Truemark_response.Codes;
+
+            CheckResult checkResult = new()
+            {
+                Inn = Inn,
+                Kpp = "",
+                Response = checkInformation
+            };
+
+            checkInformation.Description = Truemark_response.Description;
+            checkInformation.ReqId = Truemark_response.ReqId;
+            checkInformation.ReqTimestamp = Truemark_response.ReqTimestamp;
+            checkInformation.Inst = Truemark_response.Inst;
+            checkInformation.Version = Truemark_response.Version;
+
+            TrueMarkResponses.Add(checkResult);
+            
         }
 
         public FmuAnswer()
