85ba3781628da8fdb4fd40ae734f0eb96b22b7fd
Арсений Жарков <shrayky@au124.ru>
2025-08-18 14:54:39 +0700

    авто регистрация сервисов

---
+++ src/Core/FmuApiDomain/Attributes/AutoRegisterServiceAttribute.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FmuApiDomain.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class AutoRegisterServiceAttribute : Attribute
+    {
+        public ServiceLifetime Lifetime { get; }
+
+        public AutoRegisterServiceAttribute(ServiceLifetime lifetime = ServiceLifetime.Scoped)
+        {
+            Lifetime = lifetime;
+        }
+    }
+}
---
+++ src/Core/FmuApiDomain/Attributes/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace FmuApiDomain.Attributes
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAutoRegisteredServices(this IServiceCollection services, Assembly[] assemblies)
+        {
+            foreach (var assembly in assemblies)
+            {
+                var types = assembly.GetTypes()
+                    .Where(t => t.GetCustomAttribute<AutoRegisterServiceAttribute>() != null && t.IsClass && !t.IsAbstract);
+
+                foreach (var type in types)
+                {
+                    var attr = type.GetCustomAttribute<AutoRegisterServiceAttribute>()!;
+                    var interfaces = type.GetInterfaces();
+
+                    if (interfaces.Length > 0)
+                    {
+                        services.Add(new ServiceDescriptor(interfaces[0], type, attr.Lifetime));
+                    }
+                    else
+                    {
+                        services.Add(new ServiceDescriptor(type, type, attr.Lifetime));
+                    }
+                }
+            }
+        }
+    }
+}
