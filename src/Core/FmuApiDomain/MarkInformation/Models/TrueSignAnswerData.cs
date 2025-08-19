234b3386953977e14b0e3c1c0e3e5198003bc503
Арсений Жарков <shrayky@au124.ru>
2025-08-19 15:35:19 +0700

    Изменил название класса на более понятное

---
+++ src/Core/FmuApiDomain/MarkInformation/Models/TrueSignAnswerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FmuApiDomain.MarkInformation.Models
+{
+    public class TrueSignAnswerData
+    {
+        public int Code { get; set; } = 0;
+        public string Description { get; set; } = string.Empty;
+        public string ReqId { get; set; } = string.Empty;
+        public long ReqTimestamp { get; set; } = 0;
+        public string Inst { get; set; } = "";
+        public string Version { get; set; } = "";
+    }
+}
