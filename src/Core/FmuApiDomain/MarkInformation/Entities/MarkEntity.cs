67d24e4736056ade1f8c72a936f284a3d81fada6
Арсений Жарков <shrayky@au124.ru>
2025-08-20 09:25:26 +0700

    новое поле Sales - информация о всех чеках продажи марки, Добавлен статический метод Create для создания экземпляра марки

--- src/Core/FmuApiDomain/MarkInformation/Entities/MarkEntity.cs
+++ src/Core/FmuApiDomain/MarkInformation/Entities/MarkEntity.cs
@@ -13,7 +13,26 @@ namespace FmuApiDomain.MarkInformation.Entities
         public CodeDataTrueApi TrueApiCisData { get; set; } = new();
         public TrueApiAnswerData TrueApiAnswerProperties { get; set; } = new();
         public SaleData SaleData { get; set; } = new();
+        public List<SaleData> Sales { get; set; } = new();
         public bool HaveTrueApiAnswer => TrueApiAnswerProperties.ReqId != string.Empty;
         public bool IsSold => State == MarkState.Sold;
+
+        public static MarkEntity Create(string sGtin, CodeDataTrueApi markCodeData, MarkEntity existMark, TrueApiAnswerData trueApiAnswerData)
+        {
+            string state = string.IsNullOrEmpty(existMark.State) ? MarkState.Stock : existMark.State;
+
+            var entity = new MarkEntity() 
+            {
+                Id = string.IsNullOrEmpty(existMark.Id) ? sGtin : existMark.Id,
+                MarkId = sGtin,
+                State = markCodeData.Sold ? MarkState.Sold : state,
+                TrueApiCisData = markCodeData,
+                TrueApiAnswerProperties = trueApiAnswerData,
+                Sales = existMark.Sales,
+                SaleData = existMark.SaleData
+            };
+
+            return entity;
+        }
     }
 }
