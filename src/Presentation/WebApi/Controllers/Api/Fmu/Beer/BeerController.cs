26c01c7e556757b49ae27473645575b04e0732f5
Арсений Жарков <shrayky@au124.ru>
2025-08-27 16:59:42 +0700

    Добавлен контроллер для белых списков кег. Пока пустой.

---
+++ src/Presentation/WebApi/Controllers/Api/Fmu/Beer/BeerController.cs
@@ -0,0 +1,20 @@
+﻿using FmuApiDomain.Fmu.Document;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers.Api.Fmu.Beer
+{
+    [Route("api/fmu/[controller]")]
+    [Route("[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Frontol mark unit API")]
+    public class BeerController : Controller
+    {
+        [HttpPost("/beer/connect_keg")]
+        [HttpPost("/api/fmu/beer/connect_keg")]
+        public IActionResult ConnectKeg()
+        {
+            FmuAnswer answer = new();
+            return Ok(answer);
+        }
+    }
+}
