c1bf84c2fe66c6ced7e93d6464a687d0355e0733
Арсений Жарков <shrayky@au124.ru>
2025-08-27 16:38:58 +0700

    Новая информация

--- src/Presentation/WebApi/wwwroot/js/modules/Information/informationView.js
+++ src/Presentation/WebApi/wwwroot/js/modules/Information/informationView.js
@@ -10,7 +10,7 @@ export default function informationView(id) {
         rows: [
             {
                 view: "form",
-                on:{
+                on: {
                     onViewShow: init(),
                 },
                 elements: [
@@ -19,21 +19,49 @@ export default function informationView(id) {
                         id: "appVersion",
                         label: appVersion
                     },
+
                     {
                         view: "label",
                         id: "lDatabaseEngineInfo",
                         label: "Для работы fmu-api необходимо скачать и установить базу данных <a href=\"https://couchdb.apache.org\" target=\"_blank\" style=\"color: red\">Apache CouchDb.</a>",
                     },
+
                     {
                         view: "label",
                         id: "browserRecommendation",
                         label: "Для работы рекомендуется использовать браузер Vivaldi, Edge, Opera, Chrome - в общем все на базе Chromium."
                     },
+
                     {
                         view: "label",
                         id: "swaggerLink",
                         label: "<a href=\"scalar/v1\" target=\"_blank\" style=\"color: red\">Консоль запросов swagger.</a>."
                     },
+
+                    {
+                        view: "label",
+                        id: "supportLinks",
+                        label: "<strong>Поддержка проекта:</strong>"
+                    },
+
+                    {
+                        view: "label",
+                        id: "telegramGroup",
+                        label: "• &#128172 <a href=\"https://t.me/frntlsc\" target=\"_blank\" style=\"color: #0088cc\">Telegram канал поддержки</a>"
+                    },
+
+                    {
+                        view: "label",
+                        id: "gitHubPage",
+                        label: "• &#128736; <a href=\"https://github.com/shrayky/FMU-API\" target=\"_blank\" style=\"color: #0088cc\">GitHub репозиторий</a>"
+                    },
+
+                    {
+                        view: "label",
+                        id: "moneySupport",
+                        label: "• &#128176; <a href=\"https://pay.cloudtips.ru/p/1fb36b3c\" target=\"_blank\" style=\"color: #0088cc\">Поддержать проект</a>"
+                    },
+
                     {},
                 ]
             }
