1646d0e3ac280ec62e6707e41ece371edf1ae538
arseniiz <shrayky@au124.ru>
2025-09-29 22:32:17 +0700

    новые методя для сериализации и десериализации

--- src/Core/Shared/Json/JsonHelpers.cs
+++ src/Core/Shared/Json/JsonHelpers.cs
@@ -2,27 +2,58 @@
 
 namespace Shared.Json
 {
-    public class JsonHelpers
+    public static class JsonHelpers
     {
-        async public static ValueTask<T?> DeserializeAsync<T>(byte[] jData)
+        public static async ValueTask<T?> DeserializeAsync<T>(byte[] jData)
         {
             MemoryStream? stream = new(jData);
 
             return await DeserializeAsync<T>(stream);
 
         }
-        async public static ValueTask<T?> DeserializeAsync<T>(MemoryStream stream)
+        public static async ValueTask<T?> DeserializeAsync<T>(MemoryStream stream)
         {
             var data = await JsonSerializer.DeserializeAsync<T>(stream, JsonSerializeOptionsProvider.Default());
 
             return data;
         }
 
-        async public static ValueTask<T?> DeserializeAsync<T>(Stream stream)
+        public static async ValueTask<T?> DeserializeAsync<T>(Stream stream)
         {
             var data = await JsonSerializer.DeserializeAsync<T>(stream, JsonSerializeOptionsProvider.Default());
 
             return data;
         }
+
+        public static async ValueTask<T?> DeserializeAsync<T>(string json)
+        {
+            using var stream = new MemoryStream();
+            await using var writer = new StreamWriter(stream);
+            await writer.WriteAsync(json).ConfigureAwait(false);
+            await writer.FlushAsync().ConfigureAwait(false);
+
+            stream.Position = 0;
+                
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+        
+        public static async Task<string> SerializeAsync<T>(T obj)
+        {
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, obj, JsonSerializeOptionsProvider.Default());
+            stream.Position = 0;
+            using var reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
+        }
+        
+        public static async Task<string> SerializeAsync<T>(T obj, JsonSerializerOptions options)
+        {
+            using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, obj, options);
+            stream.Position = 0;
+            using var reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
+        }
+        
     }
 }
