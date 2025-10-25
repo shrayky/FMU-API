24afe8295e5fbb27b6930466d03687886b820c38
arseniiz <shrayky@au124.ru>
2025-10-25 21:18:30 +0700

    интервал обмена с центральным сервером по умолчанию 60 минут

--- src/Core/FmuApiDomain/Configuration/Options/CentralServerConnectionProperties.cs
+++ src/Core/FmuApiDomain/Configuration/Options/CentralServerConnectionProperties.cs
@@ -5,7 +5,7 @@
         public bool Enabled { get; set; } = false;
         public string Address {  get; set; } = string.Empty;
         public string Token {  get; set; } = string.Empty;
-        public int ExchangeRequestInterval { get; set; } = 600;
+        public int ExchangeRequestInterval { get; set; } = 60;
         public string Secret { get; set; } = string.Empty;
         public bool DownloadNewVersion { get; set; } = false;
     }
