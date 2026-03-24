0c781001be538d84f1187278f9f28a41a042427b
Арсений Жарков <shrayky@au124.ru>
2026-03-24 11:39:28 +0700

    11.7 версия

--- src/Core/FmuApiDomain/Constants/ApplicationInformation.cs
+++ src/Core/FmuApiDomain/Constants/ApplicationInformation.cs
@@ -1,12 +1,11 @@
-﻿namespace FmuApiDomain.Constants
+﻿namespace FmuApiDomain.Constants;
+
+public static class ApplicationInformation
 {
-    public static class ApplicationInformation
-    {
-        public const string AppName = "FMU-API";
-        public const string Manufacture = "Automation";
-        public const string Description = "Служба для проверки марок по протоколу Атол MarkUnit";
-        public const string ServiceName = "DS:FMU-API";
-        public const int AppVersion = 11;
-        public const int Assembly = 6;
-    }
+    public const string AppName = "FMU-API";
+    public const string Manufacture = "Automation";
+    public const string Description = "Служба для проверки марок по протоколу Атол MarkUnit";
+    public const string ServiceName = "DS:FMU-API";
+    public const int AppVersion = 11;
+    public const int Assembly = 7;
 }
