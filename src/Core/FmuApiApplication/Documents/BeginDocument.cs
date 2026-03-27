512d1c6649c05c7e20e306737a8962f4728559ca
Арсений Жарков <shrayky@au124.ru>
2026-03-27 12:35:37 +0700

    fix: 500 ошибка при закрытии документа note: если в чеке были позиции с оффлайн проверкой, то по ним нет информации по группе товара

--- src/Core/FmuApiApplication/Documents/BeginDocument.cs
+++ src/Core/FmuApiApplication/Documents/BeginDocument.cs
@@ -10,122 +10,124 @@ using FmuApiDomain.TrueApi.MarkData;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace FmuApiApplication.Documents
+namespace FmuApiApplication.Documents;
+
+public class BeginDocument : IFrontolDocumentService
 {
-    public class BeginDocument : IFrontolDocumentService
+    private RequestDocument Document { get; set; }
+
+    private Lazy<IDocumentRepository> TemporaryDocumentsService { get; set; }
+    private Lazy<IMemoryCache> CacheService { get; set; }
+
+    private IMarkFabric MarkFabric { get; set; }
+    private IParametersService ParametersService { get; set; }
+    private IApplicationState AppState { get; set; }
+    
+    private readonly Parameters _configuration;
+
+    private BeginDocument(RequestDocument requestDocument, IServiceProvider provider)
     {
-        private RequestDocument Document { get; set; }
+        Document = requestDocument;
 
-        private Lazy<IDocumentRepository> TemporaryDocumentsService { get; set; }
-        private Lazy<IMemoryCache> CacheService { get; set; }
-
-        private IMarkFabric MarkFabric { get; set; }
-        private IParametersService ParametersService { get; set; }
-        private IApplicationState AppState { get; set; }
+        TemporaryDocumentsService = new Lazy<IDocumentRepository>(provider.GetRequiredService<IDocumentRepository>);
+        CacheService = new Lazy<IMemoryCache>(provider.GetRequiredService<IMemoryCache>);
         
-        private readonly Parameters _configuration;
+        MarkFabric = provider.GetRequiredService<IMarkFabric>();
 
-        private BeginDocument(RequestDocument requestDocument, IServiceProvider provider)
-        {
-            Document = requestDocument;
-
-            TemporaryDocumentsService = new Lazy<IDocumentRepository>(provider.GetRequiredService<IDocumentRepository>);
-            CacheService = new Lazy<IMemoryCache>(provider.GetRequiredService<IMemoryCache>);
-            
-            MarkFabric = provider.GetRequiredService<IMarkFabric>();
-
-            ParametersService = provider.GetRequiredService<IParametersService>();
-            AppState = provider.GetRequiredService<IApplicationState>();
-            _configuration = ParametersService.Current();
-        }
-
-        private static BeginDocument CreateObject(RequestDocument requestDocument, IServiceProvider provider) 
-            => new(requestDocument, provider);
-        
-        public static IFrontolDocumentService Create(RequestDocument requestDocument, IServiceProvider provider) 
-            => CreateObject(requestDocument, provider);
-        
-
-        public async Task<Result<FmuAnswer>> ActionAsync()
-        {
-            SendDocumentToAlcoUnitAsync();
-            
-            return await BeginDocumentAsync();
-        }
-
-        private async Task<Result<FmuAnswer>> BeginDocumentAsync()
-        {
-            FmuAnswer checkResult = new();
-
-            foreach (var position in Document.Positions)
-            {
-                if (position.Marking_codes.Count == 0)
-                    continue;
-
-                foreach (var markInBase64 in position.Marking_codes)
-                {
-                    var mark = await MarkFabric.Create(position, markInBase64);
-
-                    var trueApiCisData = await mark.TrueApiData();
-
-                    if (trueApiCisData.Codes.Count == 0)
-                        continue;
-
-                    var markData = trueApiCisData.Codes[0];
-
-                    if (!markData.GroupIds.Contains(TrueApiGroup.Tobaco))
-                        continue;
-                    
-                    var minPrice = _configuration.MinimalPrices.Tabaco > markData.Smp ? _configuration.MinimalPrices.Tabaco : markData.Smp;
-
-                    if (minPrice > position.Total_price * 100)
-                    {
-                        checkResult.Code = 3;
-                        checkResult.Error += $"\r\n {position.Text} цена ниже минимальной розничной!";
-                        checkResult.Marking_codes.Add(markInBase64);
-                    }
-
-                    if (markData.Mrp < position.Total_price * 100)
-                    {
-                        checkResult.Code = 3;
-                        checkResult.Error += $"\r\n {position.Text} цена выше максимальной розничной!";
-                        checkResult.Marking_codes.Add(markInBase64);
-                    }
-
-                }
-            }
-
-            if (_configuration.Database.ConfigurationIsEnabled && AppState.CouchDbOnline())
-                await TemporaryDocumentsService.Value.Add(Document);
-            else
-                CacheService.Value.Set($"cashDoc_{Document.Uid}", Document, TimeSpan.FromMinutes(5));
-
-            return Result.Success(checkResult);
-        }
-
-        private void SendDocumentToAlcoUnitAsync()
-        {
-            var auDoc = Document;
-
-            if (_configuration.FrontolAlcoUnit.NetAdres == string.Empty)
-                return;
-
-            var positionsForDelete = new List<Position>();
-
-            foreach (var pos in auDoc.Positions)
-            {
-                if (pos.Stamps.Count > 0)
-                    continue;
-
-                if (pos.Marking_codes.Count == 0)
-                    continue;
-
-                positionsForDelete.Add(pos);
-            }
-
-            foreach (var pos in positionsForDelete)
-                auDoc.Positions.Remove(pos);
-        }
-
+        ParametersService = provider.GetRequiredService<IParametersService>();
+        AppState = provider.GetRequiredService<IApplicationState>();
+        _configuration = ParametersService.Current();
     }
+
+    private static BeginDocument CreateObject(RequestDocument requestDocument, IServiceProvider provider) 
+        => new(requestDocument, provider);
+    
+    public static IFrontolDocumentService Create(RequestDocument requestDocument, IServiceProvider provider) 
+        => CreateObject(requestDocument, provider);
+    
+
+    public async Task<Result<FmuAnswer>> ActionAsync()
+    {
+        SendDocumentToAlcoUnitAsync();
+        
+        return await BeginDocumentAsync();
+    }
+
+    private async Task<Result<FmuAnswer>> BeginDocumentAsync()
+    {
+        FmuAnswer checkResult = new();
+
+        foreach (var position in Document.Positions)
+        {
+            if (position.Marking_codes.Count == 0)
+                continue;
+
+            foreach (var markInBase64 in position.Marking_codes)
+            {
+                var mark = await MarkFabric.Create(position, markInBase64);
+
+                var trueApiCisData = await mark.TrueApiData();
+
+                if (trueApiCisData.Codes.Count == 0)
+                    continue;
+
+                var markData = trueApiCisData.Codes[0];
+
+                if (markData.GroupIds == null)
+                    continue;
+
+                if (!markData.GroupIds.Contains(TrueApiGroup.Tobaco))
+                    continue;
+                
+                var minPrice = _configuration.MinimalPrices.Tabaco > markData.Smp ? _configuration.MinimalPrices.Tabaco : markData.Smp;
+
+                if (minPrice > position.Total_price * 100)
+                {
+                    checkResult.Code = 3;
+                    checkResult.Error += $"\r\n {position.Text} цена ниже минимальной розничной!";
+                    checkResult.Marking_codes.Add(markInBase64);
+                }
+
+                if (markData.Mrp < position.Total_price * 100)
+                {
+                    checkResult.Code = 3;
+                    checkResult.Error += $"\r\n {position.Text} цена выше максимальной розничной!";
+                    checkResult.Marking_codes.Add(markInBase64);
+                }
+
+            }
+        }
+
+        if (_configuration.Database.ConfigurationIsEnabled && AppState.CouchDbOnline())
+            await TemporaryDocumentsService.Value.Add(Document);
+        else
+            CacheService.Value.Set($"cashDoc_{Document.Uid}", Document, TimeSpan.FromMinutes(5));
+
+        return Result.Success(checkResult);
+    }
+
+    private void SendDocumentToAlcoUnitAsync()
+    {
+        var auDoc = Document;
+
+        if (_configuration.FrontolAlcoUnit.NetAdres == string.Empty)
+            return;
+
+        var positionsForDelete = new List<Position>();
+
+        foreach (var pos in auDoc.Positions)
+        {
+            if (pos.Stamps.Count > 0)
+                continue;
+
+            if (pos.Marking_codes.Count == 0)
+                continue;
+
+            positionsForDelete.Add(pos);
+        }
+
+        foreach (var pos in positionsForDelete)
+            auDoc.Positions.Remove(pos);
+    }
+
 }
