806f240e6afdb432200a11f4c0d9b386a2e2c901
Арсений Жарков <shrayky@au124.ru>
2026-04-06 06:40:46 +0300

    do: версия 11-8

--- src/Core/FmuApiDomain/Constants/ApplicationInformation.cs
+++ src/Core/FmuApiDomain/Constants/ApplicationInformation.cs
@@ -7,5 +7,5 @@ public static class ApplicationInformation
     public const string Description = "Служба для проверки марок по протоколу Атол MarkUnit";
     public const string ServiceName = "DS:FMU-API";
     public const int AppVersion = 11;
-    public const int Assembly = 7;
+    public const int Assembly = 8;
 }
