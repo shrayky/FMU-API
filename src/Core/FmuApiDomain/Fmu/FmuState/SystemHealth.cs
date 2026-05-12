d11739ff00795a004eab824038d10b9616d34a71
Арсений Жарков <shrayky@au124.ru>
2026-05-12 15:28:31 +0700

    эндпоинт для проверки фронтолом статуса fmu-api

---
+++ src/Core/FmuApiDomain/Fmu/FmuState/SystemHealth.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FmuApiDomain.Fmu.FmuState;
+
+public record SystemHealth
+{
+    [JsonPropertyName("version")]
+    public string Version { get; set; } = string.Empty;
+
+    [JsonPropertyName("state")]
+    public string State { get; set; } = "regular";
+
+    [JsonPropertyName("timestamp")]
+    public long Timestamp { get; set; }
+}
