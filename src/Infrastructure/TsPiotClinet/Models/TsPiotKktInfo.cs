3a23eff5c86a026e755da9460b1a496400c77af9
Арсений Жарков <shrayky@au124.ru>
2026-05-26 11:05:58 +0700

    расширена модель с параметрами локального модуля из пиота

--- src/Infrastructure/TsPiotClinet/Models/TsPiotKktInfo.cs
+++ src/Infrastructure/TsPiotClinet/Models/TsPiotKktInfo.cs
@@ -18,4 +18,31 @@ public record TsPiotKktInfo
 
 	[JsonPropertyName("codesCheckTimeout")]
 	public int CodesCheckTimeout { get; set; } = 0;
+
+	[JsonPropertyName("lm")]
+	public LocalModule LocalModeStatus { get; set; } = new();
+}
+
+public record LocalModule
+{
+	[JsonPropertyName("version")]
+	public string Version { get; set; } = string.Empty;
+
+	[JsonPropertyName("status")]
+	public string Status { get; set; } = string.Empty;
+
+    [JsonPropertyName("lastSync")]
+	public long LastSyncTime { get; set; } = 0;
+
+	[JsonPropertyName("token")]
+	public string Token { get; set; } = string.Empty;
+
+    [JsonPropertyName("expDate")]
+	public string TokenExpirationDate {  get; set; } = string.Empty; // Дата и время истечения срока действия токена в формате ISO 8601 (например, 2025-03-22T10:30:00Z). Время указано в UTC.
+
+    [JsonPropertyName("ip")]
+	public string Ip {  get; set; } = string.Empty;
+
+	[JsonPropertyName("port")]
+	public int Port { get; set; } = 0;
 }
