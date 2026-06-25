88edc0c4b9609e5f5a9e35a8d4fc0f218bcff2ef
Арсений Жарков <shrayky@au124.ru>
2026-06-25 14:06:15 +0700

    new: по умолчанию - режим работы тс пиот

--- src/Core/FmuApiDomain/Configuration/Options/ServerConfig.cs
+++ src/Core/FmuApiDomain/Configuration/Options/ServerConfig.cs
@@ -3,6 +3,6 @@
 public class ServerConfig
 {
     public int ApiIpPort { get; set; } = 2578;
-    public bool TsPiotEnabled { get; set; } = false;
+    public bool TsPiotEnabled { get; set; } = true;
     public int LocalModuleVersion { get; set; } = 2;
 }
