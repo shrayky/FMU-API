f7c72caabe3a1a5c2bac519c6c7bc6387d805daa
Арсений Жарков <shrayky@au124.ru>
2026-08-11 15:31:06 +0700

    fix: опечатки

--- src/Presentation/WebApi/wwwroot/js/modules/settings/elements/organizationsTable.js
+++ src/Presentation/WebApi/wwwroot/js/modules/settings/elements/organizationsTable.js
@@ -329,8 +329,8 @@ class OrganizationsConfigurationElement {
         if (!item.localModuleConnection?.enable)
             return;
 
-        if (item.localModuleStatus === this.LOCAL_MODULE_STATUS.NOT_CONFIGURED)
-            || (item.localModuleStatus === this.LOCAL_MODULE_STATUS.SYNC_ERROR) {
+        if (item.localModuleStatus === this.LOCAL_MODULE_STATUS.NOT_CONFIGURED
+            || item.localModuleStatus === this.LOCAL_MODULE_STATUS.SYNC_ERROR) {
             this._startInitialization(selectedId, item);
             return;
         }
